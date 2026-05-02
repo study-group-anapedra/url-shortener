5e419a6b776b9760cfe5b46e24adc0096c7c7367
anapedra <anapedra.mil@gmail.com>
2026-05-02 15:12:25 -0300

    chore: update API endpoint configuration (prepare for custom domain)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://rgujyvz8jf.execute-api.us-east-1.amazonaws.com/prod/url";
+const API_URL = "https://api.asantanadev.com/prod/url";
 export async function encurtarUrl(originalUrl) {
   const response = await fetch(API_URL, {
     method: "POST",
